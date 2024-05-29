8d496b00746160eca31f0d38bf8d916b87160438
Gleidson Neves da Silveira <gleidisonmt@gmail.com>
2024-05-29 09:22:16 -0300

    upate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
-import Stack from "@mui/material/Stack";
 import "./App.css";
-import { Button } from "@mui/material";
-import ResponsiveDrawer from "./components/ResponsiveDrawer";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <ResponsiveDrawer />;
+  return (
+    <div class="flex justify-start">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <button className="text-red-500">My Button</button>
+    </div>
+  );
 }
 
 export default App;
